34b8db284fd4d176d1667a0d174e9d6a31f03cc6
Ashwath Sampath <ashwath92@gmail.com>
2020-05-31 00:32:53 +0000

    Reset -- back to the start

--- dags/sparkify_dag.py
+++ dags/sparkify_dag.py
@@ -16,7 +16,7 @@ default_args = {
     'depends_on_past': False,
     'start_date': datetime(2018, 11, 1),
     'email': ['ashwath92@gmail.com'],
-    'email_on_failure': True,
+    'email_on_failure': False,
     'email_on_retry': False,
     #'retries': 3,
     #'retry_delay': timedelta(minutes=5),
@@ -44,88 +44,44 @@ default_args = {
 dag = DAG('sparkify_elt_dag',
           default_args=default_args,
           description='Load and transform data in Redshift with Airflow',
-          #schedule_interval='0 * * * *'
+          schedule_interval='0 * * * *'
         )
 
-#start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
+start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
 
-create_tables_task = PostgresOperator(
-    task_id='create_tables',
-    dag=dag,
-    #sql in same directory
-    sql='create_tables.sql',
-    postgres_conn_id='redshift'
-)
-'''
 stage_events_to_redshift = StageToRedshiftOperator(
     task_id='Stage_events',
-    dag=dag,
-    redshift_conn_id='redshift',
-    aws_credentials_id='aws_credentials',
-    s3_bucket='udacity-dend',
-    s3_key='log_data',
-    region='us-west-2',
-    destination_table='events',
-    input_file_type='json',
-    provide_context=True
+    dag=dag
 )
 
 stage_songs_to_redshift = StageToRedshiftOperator(
     task_id='Stage_songs',
-    dag=dag,
-    redshift_conn_id='redshift',
-    aws_credentials_id='aws_credentials',
-    s3_bucket='udacity-dend',
-    s3_key='song_data',
-    region='us-west-2',
-    destination_table='events',
-    input_file_type='json',
-    provide_context=True
+    dag=dag
 )
 
 load_songplays_table = LoadFactOperator(
     task_id='Load_songplays_fact_table',
-    dag=dag,
-    redshift_conn_id='redshift',
-    aws_credentials_id='aws_credentials',
-    sql=SqlQueries.songplay_table_insert,
-    provide_context=True
+    dag=dag
 )
 
 load_user_dimension_table = LoadDimensionOperator(
     task_id='Load_user_dim_table',
-    dag=dag,
-    redshift_conn_id='redshift',
-    aws_credentials_id='aws_credentials',
-    destination_table='users',
-    sql=SqlQueries.user_table_insert
+    dag=dag
 )
 
 load_song_dimension_table = LoadDimensionOperator(
     task_id='Load_song_dim_table',
-    dag=dag,
-    redshift_conn_id='redshift',
-    aws_credentials_id='aws_credentials',
-    destination_table='songs',
-    sql=SqlQueries.song_table_insert
+    dag=dag
 )
 
 load_artist_dimension_table = LoadDimensionOperator(
     task_id='Load_artist_dim_table',
-    dag=dag,
-    redshift_conn_id='redshift',
-    aws_credentials_id='aws_credentials',
-    destination_table='artists',
-    sql=SqlQueries.artist_table_insert
+    dag=dag
 )
 
 load_time_dimension_table = LoadDimensionOperator(
     task_id='Load_time_dim_table',
-    dag=dag,
-    redshift_conn_id='redshift',
-    aws_credentials_id='aws_credentials',
-    destination_table='time',
-    sql=SqlQueries.time_table_insert
+    dag=dag
 )
 
 run_quality_checks = DataQualityOperator(
@@ -134,9 +90,3 @@ run_quality_checks = DataQualityOperator(
 )
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
-#start_operator >> create_tables_task 
-create_tables_task >> [stage_events_to_redshift, stage_songs_to_redshift] >> load_songplays_table
-load_songplays_table >> [load_artist_dimension_table, load_song_dimension_table, load_user_dimension_table, load_time_dimension_table] >> run_quality_checks
-#[load_artist_dimension_table, load_song_dimension_table, load_user_dimension_table, load_time_dimension_table] >> run_quality_checks
-run_quality_checks >> end_operator
-'''
--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -11,22 +11,12 @@ class LoadDimensionOperator(BaseOperator):
                  # Define your operators params (with defaults) here
                  # Example:
                  # conn_id = your-connection-name
-                 redshift_conn_id="",
-                 aws_credentials_id="",
-                 destination_table="",
-                 sql="",
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
         # Map params here
         # Example:
         # self.conn_id = conn_id
-        self.redshift_conn_id = redshift_conn_id
-        self.aws_credentials_id = aws_credentials_id
-        self.sql=sql
 
     def execute(self, context):
-        #self.log.info('LoadDimensionOperator not implemented yet')
-        redshift_hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
-        redshift_hook.run(self.sql)
-        self.log.info()
+        self.log.info('LoadDimensionOperator not implemented yet')
